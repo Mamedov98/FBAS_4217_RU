a89e66a032cdda14837d566ec4731522adb5f3fa
Azimov Elvin <e.azimov@itstep.edu.az>
2022-05-20 17:06:39 +0400

    Operator overloading

---
+++ OOP/Lesson4/Lesson4/Main.cpp
@@ -0,0 +1,19 @@
+#include "Person.h"
+#include <iostream>
+using namespace std;
+
+
+
+
+int main() {
+
+	Person p1(new char[] {"Elvin"}, new char[] {"Azimov"}, 20);
+	Person* p2 = new Person(new char[] {"Mamed"}, new char[] {"Kerimli"}, 21);
+
+	//cout << p1; // cout - std::ostream, << - operator, p1 - &Person 
+
+	cout << p1;
+	p1++;
+	cout << p1;
+}
+
