35d87c247bf23586ef3c1a51cfd837f482eb1fe2
elvin <elvin.azim01@gmail.com>
2022-06-13 15:51:10 +0400

    Printers part 1 and linked list full version

--- OOP/DataStructures/SinglyLinkedList/SinglyLinkedList/SinglyLinkedList.cpp
+++ OOP/DataStructures/SinglyLinkedList/SinglyLinkedList/SinglyLinkedList.cpp
@@ -3,49 +3,79 @@ using namespace std;
 
 
 template <typename T>
-class List
+class list
 {
 public:
-	struct Node
+	struct node
 	{
 		T value{};
-		Node* next{};
+		node* next{};
+
+		node() {  }
+		node(T value, node* next)
+		{
+			this->value = value;
+			this->next = next;
+		}
 	};
 
-	List(size_t size)
+	explicit list(size_t size)
 	{
 		int i = 1;
 
-		this->size = size;
-		this->head = new List<T>::Node;
+		this->size_ = size;
+		this->head_ = new list<T>::node;
 
 		cout << "Enter " << i << "element: ";
-		cin >> this->head->value;
+		cin >> this->head_->value;
 
-		Node* cur_elem = this->head;
+		node* cur_elem = this->head_;
 		i++;
 
 		while (i <= size)
 		{
 			cout << "Enter " << i << "element: ";
-			cur_elem->next = new List<T>::Node;
+			cur_elem->next = new list<T>::node;
 
 			cin >> cur_elem->next->value;
 			cur_elem = cur_elem->next;
 
 			if (i == size)
 			{
-				this->tail = new List<T>::Node;
-				this->tail = cur_elem;
+				this->tail_ = new list<T>::node;
+				this->tail_ = cur_elem;
 			}
 			i++;
 		}
 	}
-
-
-	friend ostream& operator<<(ostream& out, List<T>& list)
+	list(std::initializer_list<T> init)
 	{
-		Node* cur_elem = list.head;
+		this->size_ = init.size();
+		this->tail_ = new node(*(init.end() - 1), nullptr);
+
+		for (auto *i = init.end() - 1; i >= init.begin(); --i)
+			this->add_to_head(*i);
+	}
+
+	size_t get_size() const { return this->size_; }
+	node* get_head() const { return this->head_; }
+	node* get_tail() const { return this->tail_; }
+
+	void add_to_head(T value)
+	{
+		auto* new_node = new node(value, this->get_head());
+		this->head_ = new_node;
+	}
+	void add_to_tail(T value)
+	{
+		auto* new_node = new list<T>::node(value, nullptr);
+		this->tail_->next = new_node;
+		this->tail_ = new_node;
+	}
+
+	friend ostream& operator<<(ostream& out, list<T>& list)
+	{
+		node* cur_elem = list.head_;
 		while (cur_elem != nullptr)
 		{
 			out << cur_elem->value << endl;
@@ -54,22 +84,17 @@ public:
 		return out;
 	}
 
-	size_t get_size() { return this->size; }
-	Node* get_head() { return this->head; }
-	Node* get_tail() { return this->tail; }
-
 private:
-	Node* head{};
-	Node* tail{};
-	size_t size;
+	node* head_{};
+	node* tail_{};
+	size_t size_{};
 };
 
 
 int main()
 {
-	List<int> my_list(3);
+	list<int> my_list {1, 2, 3, 4, 5};
 	cout << my_list << endl;
-
 
 	cout << "Head: " << my_list.get_head()->value << endl;
 	cout << "Tail: " << my_list.get_tail()->value << endl;
