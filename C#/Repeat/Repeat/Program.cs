c3cfa09c50f83a017c8da8141c049dbad9c7c5c8
Elvin <elvin.azim01@gmail.com>
2022-08-29 15:47:24 +0400

    Interfaces and operator overloading

---
+++ C#/Repeat/Repeat/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Repeat;
+
+Car a = new() { Make = "Mercedes", Year = 2000 };
+Car b = new() { Year = 2017 };
+
+Console.WriteLine(a < b);
+
+Showroom showroom = new() { Cars = new[] {a, b}};
+
+showroom[1] = new Car();
+
+Console.WriteLine(showroom[0]);
+Console.WriteLine(showroom[1]);
---
+++ C#/WeatherApp/WeatherApp/IForecastService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp
+{
+    internal interface IForecastService
+    {
+        public string GetForecastByCity(string city);
+    }
+}
