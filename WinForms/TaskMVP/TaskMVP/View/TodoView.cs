2897f50a542bea1836cc1c6c4f86bbc27e1d73a7
Elvin <elvin.azim01@gmail.com>
2022-10-07 17:43:26 +0400

    MVP lesson2

--- WinForms/TaskMVP/TaskMVP/View/TodoView.cs
+++ WinForms/TaskMVP/TaskMVP/View/TodoView.cs
@@ -1,5 +1,6 @@
 using TaskMVP.Model;
 using TaskMVP.Presenter;
+using TaskMVP.View;
 
 namespace TaskMVP
 {
@@ -27,6 +28,26 @@ namespace TaskMVP
 
         private void task_listBox_SelectedIndexChanged(object sender, EventArgs e)
         {
+        }
+
+        private void delete_button_Click(object sender, EventArgs e)
+        {
+            var selected = task_listBox.SelectedItems;
+
+            for (int i = 0; i < selected.Count; i++)
+            {
+                task_listBox.Items.Remove(selected[i] as Todo);
+            }
+        }
+
+        private void edit_button_Click(object sender, EventArgs e)
+        {
+            var editWindow = new EditView(task_listBox.SelectedItem as Todo);
+
+            editWindow.Show();
+            
+            // editWindow.ShowDialog();
+            // this.WindowState = FormWindowState.Minimized;
         }
     }
 }
