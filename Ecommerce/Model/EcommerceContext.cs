6aa01f6f8981eda9df3a9cbb522e9e8a9f02e833
Mamedov98 <110538493+Mamedov98@users.noreply.github.com>
2023-03-21 16:16:36 +0400

    initial

---
+++ Ecommerce/Model/EcommerceContext.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace Ecommerce.Model
+{
+    internal class EcommerceContext : DbContext
+    {
+        public DbSet<Brand> brands { get; set; }
+        public DbSet<Sneaker> sneakers { get; set; }
+        public DbSet<Photo> photos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=EcommerceDataNew;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           modelBuilder.Entity<Sneaker>()
+               
+            .HasOne(s => s.Brand)
+            .WithMany(b => b.Sneakers)
+            .HasForeignKey(s => s.BrandId)
+            .OnDelete(DeleteBehavior.Cascade );
+
+            modelBuilder.Entity<Photo>()
+                .HasOne(p => p.Sneakers)
+                .WithMany(s => s.Photos)
+                .HasForeignKey(p => p.SneakerId )
+                .OnDelete(DeleteBehavior.Cascade );
+
+            modelBuilder.Entity<Sneaker>()
+               .Property(s => s.Price)
+               .HasColumnType("decimal(18, 2)"); 
+        }
+
+        public class Brand
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public virtual ICollection<Sneaker> Sneakers { get; set; }
+        }
+
+        public class Sneaker
+        {
+            public int Id { get; set; }
+            public int BrandId { get; set; }
+            public string Name { get; set; }
+            public decimal Price { get; set; }
+            public virtual Brand Brand { get; set; }
+            public virtual ICollection<Photo> Photos { get; set; }
+        }
+        public class Photo
+        {
+            public int Id { get; set; }
+            public int SneakerId { get; set; }
+            public string PhotoData { get; set; }
+            public virtual Sneaker Sneakers { get; set; }
+        }
+    }
+}
---
+++ Ecommerce/ViewModel/SneakersViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+using static Ecommerce.Model.EcommerceContext;
+using Ecommerce.Model;
+
+namespace Ecommerce.ViewModel
+{
+    class SneakersViewModel
+    {
+        public ObservableCollection<Sneaker> Sneakers { get; set; }
+        public Photo Photo { get; set; } = new()
+        {
+            PhotoData = @"https://wallpapercave.com/wp/wp4312523.jpg"
+        };
+
+
+        public SneakersViewModel()
+        {
+            using (var context = new EcommerceContext())
+            {
+                // Создаем бренд и добавляем в контекст
+                var brand = new Brand { Name = "Nike" };
+                context.brands.Add(brand);
+                context.SaveChanges();
+
+                // Создаем кроссовки и добавляем в контекст
+                var sneaker = new Sneaker
+                {
+                    Name = "Air Jordan",
+                    BrandId = brand.Id, // указываем связанный бренд
+                    Price = 150m
+                };
+               context.sneakers.Add(sneaker);
+
+               //Сохраняем изменения в базе данных
+                context.SaveChanges();
+            }
+        }
+    }
+}
